c17b01fcf03c5d5aadb5d3b279309f552e858501
arsha-123 <arshaashok850@gmail.com>
2022-01-18 13:29:46 +0530

    2nd commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn project9.wsgi --log-file -
--- project9/settings.py
+++ project9/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5s0y(%47lyxnd72*a2r_2cm^8ff^q%ve=rt_=sz6xs1*w&j+(d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
