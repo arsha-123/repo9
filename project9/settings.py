1352d6cbe22909ff829a14a05b1cb76c1e96d50c
arsha-123 <arshaashok850@gmail.com>
2022-01-18 14:40:51 +0530

    added whitenoise line

--- project9/settings.py
+++ project9/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5s0y(%47lyxnd72*a2r_2cm^8ff^q%ve=rt_=sz6xs1*w&j+(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = true
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['app999999.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
