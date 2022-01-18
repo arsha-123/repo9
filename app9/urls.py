fc9584220584dc2c6f572aeb81b7ef61771f5954
arsha-123 <arshaashok850@gmail.com>
2022-01-18 11:51:42 +0530

    1st commit

---
+++ app9/urls.py
@@ -0,0 +1,6 @@
+from . import views
+from django.urls import path
+
+urlpatterns=[
+    path('fam9',views.fam9)
+]
---
+++ app9/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def fam9(request):
+    return render(request,'index9.html')
